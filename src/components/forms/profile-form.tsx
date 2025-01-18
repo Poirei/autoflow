02b7e04ace8c07144e051016fcfd0245a16bec75
poirei <poirei4@gmail.com>
2025-01-18 17:09:39 +0530

    added prisma

---
+++ src/components/forms/profile-form.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { EditUserProfileSchema } from "@/lib/types";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+
+const ProfileForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof EditUserProfileSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(EditUserProfileSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-6 md:w-1/3" onSubmit={() => {}}>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Username</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={true}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="self-start hover:bg-[#2F006B] hover:text-white hover:drop-shadow-sm hover:shadow-[#2F006B]/40 transition-all duration-200 ease-in-out"
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="h-4 w-4 animate-spin" />
+              <span>Saving</span>
+            </>
+          ) : (
+            "Save Changes"
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
