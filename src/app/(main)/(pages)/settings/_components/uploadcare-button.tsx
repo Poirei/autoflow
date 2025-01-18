477c2f7fd0a0f502e4c476960ad4d9b2793f02a6
poirei <poirei4@gmail.com>
2025-01-18 18:06:37 +0530

    added clerk and uploadcare

---
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -0,0 +1,13 @@
+"use client";
+import "@uploadcare/react-uploader/core.css";
+import { FileUploaderRegular } from "@uploadcare/react-uploader/next";
+
+type Props = {
+  onUpload?: any;
+};
+
+const UploadCareButton = (props: Props) => {
+  return <div>UploadCareButton</div>;
+};
+
+export default UploadCareButton;
--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,9 +1,22 @@
 import ProfileForm from "@/components/forms/profile-form";
-import React from "react";
+import ProfilePicture from "./_components/profile-picture";
+import { db } from "@/lib/db";
 
-type Props = {};
+const Page = () => {
+  const removeProfileImage = async () => {
+    "use server";
 
-const Page = (props: Props) => {
+    const response = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        profileImage: null,
+      },
+    });
+
+    return response;
+  };
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -16,6 +29,7 @@ const Page = (props: Props) => {
             Add or update your information
           </p>
         </div>
+        <ProfilePicture />
         <ProfileForm />
       </div>
     </div>
